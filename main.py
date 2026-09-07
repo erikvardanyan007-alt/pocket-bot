bbec72c8d5e7f6911d3198f6869973c38de17323
erikvardanyan007-alt <erik.vardanyan007@gmail.com>
2026-09-07 06:31:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ async def run_bridge():
         asset = data.get("asset")
         price = data.get("close")
         timestamp = data.get("time")
-        
         print(f"[TICK] Asset: {asset} | Price: {price} | Time: {timestamp}")
 
     await po_client.connect()
